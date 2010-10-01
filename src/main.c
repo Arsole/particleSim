a494cb207a3787cbcd66648ac7c189cd192f245a
Felix Wallin <nibbler.v1@gmail.com>
2010-10-01 15:24:19 +0200

    uhm

--- src/main.c
+++ src/main.c
@@ -8,6 +8,19 @@
 #define Y_RES 400
 #define X_RES 600
 typedef unsigned int temp;
+struct node
+{
+    struct node * next;
+    particle * type;
+};
+typedef struct node node;
+struct list
+{
+    node * head;
+};
+typedef struct list list;
+
+void add_element (particle * type, 
 struct particle
 {
     uint32_t mask;
@@ -16,7 +29,7 @@ struct particle
     temp m_point; // Melting point
     temp b_point; // Boiling point
     temp p_point; // Plasma point, could use a better name
-    
+    uint32_t color;
 };
 typedef struct particle particle;
 
@@ -45,6 +58,8 @@ main (int argc, char **argv)
     printf ("Using %d threads", num_threads); 
     particle * dust = malloc (sizeof (particle));
     dust->temp_p = 273;
+    dust->color = 255+256+256;
     grid[Y_RES/2][X_RES/2] = construct_particle (dust);
+    printf ("Color of particle: %d", dust->color);
     return 0;
 }
